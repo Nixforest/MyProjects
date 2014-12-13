5ad0e387f4beb4ab7f7caf8b077fc33e7e715083
Nixforest <Nixforest21991920@gmail.com>
2014-12-13 10:35:22 +0700

    [General] Commit LogManager project

---
+++ logmanager/logmanager/GlobalVariable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace logmanager
+{
+    /// <summary>
+    /// Class Global Variable.
+    /// </summary>
+    public class GlobalVariable
+    {
+        #region Config value
+        /// <summary>
+        /// File path of Log file.
+        /// </summary>
+        public static string LOG_FILE_PATH = "";
+
+        /// <summary>
+        /// Date time format to log file
+        /// </summary>
+        public static string DATE_TIME_FORMAT = "{0:yyyy/MM/dd HH:mm:ss}";
+
+        /// <summary>
+        /// Log content format
+        /// </summary>
+        public static string LOG_CONTENT_FORMAT = "[{0, -19}][{1, -50}][{2, -50}][{3, 6}] {4}";
+        #endregion
+
+        #region Global String
+        /// <summary>
+        /// Error string
+        /// </summary>
+        public static string ERR_ERROR = "Error";
+
+        /// <summary>
+        /// String: Load config file error
+        /// </summary>
+        public static string ERR_LOAD_CONFIG_FILE = "Load configuration file error";
+
+        /// <summary>
+        /// String: Load log file error: Unanthorized
+        /// </summary>
+        public static string ERR_LOAD_LOG_FILE_UNAUTHORIZED = "Load configuration file error: Can not access";
+
+        /// <summary>
+        /// String: Load log file error: Directory not found
+        /// </summary>
+        public static string ERR_LOAD_LOG_FILE_DIRECTORY_NOT_FOUND = "Load configuration file error: Directory not found";
+        #endregion
+    }
+}
---
+++ logmanager/logmanager/LogManagement.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace logmanager
+{
+    public class LogManagement
+    {
+        /// <summary>
+        /// Class for LOG handle
+        /// Log file format:
+        /// [Date][FileName][Function][Line][Message]
+        /// </summary>
+        public static void WriteLogFile(string log)
+        {
+            try
+            {
+                using (StreamWriter file = new StreamWriter(GlobalVariable.LOG_FILE_PATH, true))
+                {
+                    file.WriteLine(log);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show(GlobalVariable.ERR_LOAD_LOG_FILE_UNAUTHORIZED,
+                    GlobalVariable.ERR_ERROR,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show(GlobalVariable.ERR_LOAD_LOG_FILE_DIRECTORY_NOT_FOUND,
+                   GlobalVariable.ERR_ERROR,
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Error);
+            }
+            catch (PathTooLongException)
+            {
+                MessageBox.Show(GlobalVariable.ERR_LOAD_LOG_FILE_DIRECTORY_NOT_FOUND,
+                   GlobalVariable.ERR_ERROR,
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,
+                   GlobalVariable.ERR_ERROR,
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Write message in format:
+        /// [Date][FileName][Function][Line][Message]
+        /// </summary>
+        /// <param name="fileName">File name</param>
+        /// <param name="function">Function name</param>
+        /// <param name="line">Line of code</param>
+        /// <param name="msg">Message content</param>
+        public static void WriteMessageFile(string fileName, string function, int line, string msg)
+        {
+            string date = String.Format(GlobalVariable.DATE_TIME_FORMAT, DateTime.Now);
+            string log = String.Format(
+                GlobalVariable.LOG_CONTENT_FORMAT,
+                date, fileName, function, line, msg);
+            WriteLogFile(log);
+        }
+
+        /// <summary>
+        /// Write message in format:
+        /// [Date][FileName][Function][Line][Message]
+        /// </summary>
+        /// <param name="msg">Message content</param>
+        /// <param name="stackTrace">StackTrace object</param>
+        public static void WriteMessageFile(string msg, StackTrace stackTrace)
+        {
+            string fileName = Path.GetFileName(stackTrace.GetFrame(0).GetFileName());
+            string function = stackTrace.GetFrame(0).GetMethod().Name;
+            int line = stackTrace.GetFrame(0).GetFileLineNumber();
+            WriteMessageFile(fileName, function, line, msg);
+        }
+    }
+}
