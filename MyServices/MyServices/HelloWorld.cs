04a6ae80aaa865fb516f04950b0936f1819dc6a6
Nixforest <Nixforest21991920@gmail.com>
2014-12-13 11:10:53 +0700

    [Game] Commit Pacman project

---
+++ MyServices/MyServices/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HelloWorld" in both code and config file together.
+    public class HelloWorld : IHelloWorld
+    {
+        #region IHelloWorld Members
+        public string SayHello(string inputName)
+        {
+            return "Hello " + inputName;
+        }
+        #endregion
+    }
+}
---
+++ Pacman3D/ReadMe.txt
@@ -0,0 +1,3 @@
+Use Up, Down, Left, Right key to control pacman.
+Use F1, F2, F3, F4 key to control camera.
+Nixforest!
